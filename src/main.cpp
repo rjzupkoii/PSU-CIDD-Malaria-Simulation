d15233e9669386d649964f56542979853cb919dd
Nguyen Tran <merlinvn@gmail.com>
2018-07-24 14:14:48 -0400

    add output job number

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,9 @@
 #include "args.hxx"
 #include "Helpers/OSHelpers.h"
 
+
+int job_number = 0;
+
 INITIALIZE_EASYLOGGINGPP
 
 using namespace std;
@@ -38,21 +41,21 @@ void config_logger() {
   el::Configurations monthly_reporter_logger;
   monthly_reporter_logger.setToDefault();
   monthly_reporter_logger.set(el::Level::Debug, el::ConfigurationType::Format,
-                      "[%level] [%logger] [%host] [%func] [%loc] %msg");
+                              "[%level] [%logger] [%host] [%func] [%loc] %msg");
   monthly_reporter_logger.set(el::Level::Error, el::ConfigurationType::Format,
-                      "[%level] [%logger] [%host] [%func] [%loc] %msg");
+                              "[%level] [%logger] [%host] [%func] [%loc] %msg");
   monthly_reporter_logger.set(el::Level::Fatal, el::ConfigurationType::Format,
-                      "[%level] [%logger] [%host] [%func] [%loc] %msg");
+                              "[%level] [%logger] [%host] [%func] [%loc] %msg");
   monthly_reporter_logger.set(el::Level::Trace, el::ConfigurationType::Format,
-                      "[%level] [%logger] [%host] [%func] [%loc] %msg");
+                              "[%level] [%logger] [%host] [%func] [%loc] %msg");
   monthly_reporter_logger.set(el::Level::Info, el::ConfigurationType::Format, "%msg");
   monthly_reporter_logger.set(el::Level::Warning, el::ConfigurationType::Format, "[%level] [%logger] %msg");
   monthly_reporter_logger.set(el::Level::Verbose, el::ConfigurationType::Format, "[%level-%vlevel] [%logger] %msg");
 
 
   monthly_reporter_logger.setGlobally(el::ConfigurationType::ToFile, "true");
-  monthly_reporter_logger.setGlobally(el::ConfigurationType::Filename, "monthly_data.txt");
-  monthly_reporter_logger.setGlobally(el::ConfigurationType::ToStandardOutput, "false");  
+  monthly_reporter_logger.setGlobally(el::ConfigurationType::Filename, fmt::format("monthly_data_{}.txt",job_number));
+  monthly_reporter_logger.setGlobally(el::ConfigurationType::ToStandardOutput, "false");
   monthly_reporter_logger.setGlobally(el::ConfigurationType::LogFlushThreshold, "100");
   // default logger uses default configurations
   el::Loggers::reconfigureLogger("monthly_reporter", monthly_reporter_logger);
@@ -64,12 +67,12 @@ void config_logger() {
 int main(const int argc, char** argv) {
   try {
 
-    config_logger();
-
-    START_EASYLOGGINGPP(argc, argv);
 
     auto* m = new Model();
     handle_cli(m, argc, argv);
+
+    config_logger();
+    START_EASYLOGGINGPP(argc, argv);
 
     m->initialize();
 
@@ -81,10 +84,10 @@ int main(const int argc, char** argv) {
     std::cout << __FUNCTION__ << "-" << e.what() << std::endl;
     return 1;
   }
-
-
   return 0;
 }
+
+
 
 void handle_cli(Model* model, int argc, char** argv) {
   args::ArgumentParser parser("Individual-based simulation for malaria.", "uut47@psu.edu");
@@ -93,6 +96,9 @@ void handle_cli(Model* model, int argc, char** argv) {
                                           "The config file (YAML format). \nEx: malariasimulation -i input.yml",
                                             {'i', 'c', "input", "config"});
 
+  args::ValueFlag<int> cluster_job_number(parser, "int",
+                                          "Cluster job number. \nEx: malariasimulation -j 1",
+                                            {'j'});
   try {
     parser.ParseCLI(argc, argv);
   }
@@ -108,6 +114,8 @@ void handle_cli(Model* model, int argc, char** argv) {
   }
 
   const auto input = input_file ? args::get(input_file) : "input.yml";
+
+  job_number = cluster_job_number ? args::get(cluster_job_number) : 0;
 
   if (input != "input.yml") {
     LOG(INFO) << fmt::format("Used input file: {0}", input);
