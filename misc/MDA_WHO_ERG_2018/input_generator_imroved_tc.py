a56a26e7922f236e10ae9739296a43d54531f45b
Nguyen Tran <merlinvn@gmail.com>
2018-08-13 22:14:18 -0400

    fix pfpr1

--- misc/MDA_WHO_ERG_2018/input_generator_imroved_tc.py
+++ misc/MDA_WHO_ERG_2018/input_generator_imroved_tc.py
@@ -44,12 +44,12 @@ number_MDA_round = [0,1,2,3,4];
 #    if event['name'] == 'single_round_MDA':
 #        data['events'][index]['info'] = data['events'][index]['info'][0:number_MDA_round]
 
-betas = [0.19, 0.08, 0.065, 0.05]
+betas = [0.19, 0.08, 0.065, 0.06]
 
 pfpr = {0.19: 'PFPR15',
         0.08: 'PFPR5',
         0.065: 'PFPR3',
-        0.05: 'PFPR1'}
+        0.06: 'PFPR1'}
 
 for mda_round in number_MDA_round:
     for beta in betas:
