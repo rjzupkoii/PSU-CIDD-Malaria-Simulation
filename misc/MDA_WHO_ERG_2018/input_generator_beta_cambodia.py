6a270efdecd6d33029428a2b0e0c576757f962dc
Nguyen Tran <merlinvn@gmail.com>
2018-09-26 12:01:38 -0400

    Cambodia beta 2

--- misc/MDA_WHO_ERG_2018/input_generator_beta_cambodia.py
+++ misc/MDA_WHO_ERG_2018/input_generator_beta_cambodia.py
@@ -14,13 +14,13 @@ p = inflect.engine();
 stream = open('input_Cambodia.yml', 'r');
 data = yaml.load(stream);
 #%%
-original_beta = [0.13356084,0.15162966,0.09243984,0.16028422,0.20697847,0.15419583,0.10739227,0.09094083,0.16582377,0.23753038,0.21442202,0.096879,0.19005938,0.13223566,0.13402116,0.30217877,0.14398083,0.16429467,0.17459542,0.15144762,0.14584802,0.17901,0.08933166,0.13688217,0.21134582];
+#original_beta = [0.13356084,0.15162966,0.09243984,0.16028422,0.20697847,0.15419583,0.10739227,0.09094083,0.16582377,0.23753038,0.21442202,0.096879,0.19005938,0.13223566,0.13402116,0.30217877,0.14398083,0.16429467,0.17459542,0.15144762,0.14584802,0.17901,0.08933166,0.13688217,0.21134582];
 
 def loguniform(low=0, high=1, size=None):
     return np.exp(np.random.uniform(low, high, size))
 
 def ran_beta(alpha=2, beta=1, size=None):
-    return np.random.beta(alpha, beta, size)*0.4 + 0.02
+    return np.random.beta(alpha, beta, size)*0.1 + 0.01
 
 #%%
 
@@ -35,7 +35,7 @@ for i in range(number_of_beta_sets):
 #%%
 for index,beta in enumerate(betas):
     data['location_db']['beta_by_location'] = np.full(number_of_locations, beta).tolist()
-    output_filename = 'Cambodia/pre1/beta/input_beta_%d.yml'%index;
+    output_filename = 'Cambodia/pre2/beta/input_beta_%d.yml'%index;
     output_stream = open(output_filename, 'w');
     yaml.dump(data, output_stream);
     output_stream.close();
@@ -44,7 +44,7 @@ for index,beta in enumerate(betas):
 
 import numpy
 a = numpy.asarray(betas)
-numpy.savetxt("Cambodia/pre1/beta.csv", a, delimiter=",")
+numpy.savetxt("Cambodia/pre2/beta.csv", a, delimiter=",")
 #
 #
 #print(kFormatter(9000));
