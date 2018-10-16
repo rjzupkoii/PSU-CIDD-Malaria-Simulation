ab6faa16deeab72fb015b20b35ead3a1f609d0df
Nguyen Tran <merlinvn@gmail.com>
2018-10-15 23:17:11 -0400

    add DxGGenerator project

---
+++ src/DxGGenerator/IndividualsFileReporter.h
@@ -0,0 +1,36 @@
+/* 
+ * File:   IndividualsFileReporter.h
+ * Author: Merlin
+ *
+ * Created on November 7, 2014, 11:06 AM
+ */
+
+#ifndef INDIVIDUALSFILEREPORTER_H
+#define	INDIVIDUALSFILEREPORTER_H
+
+#include "Core/PropertyMacro.h"
+#include <fstream>
+#include <string>
+#include "Reporters/Reporter.h"
+
+class IndividualsFileReporter : public Reporter {
+DISALLOW_COPY_AND_ASSIGN(IndividualsFileReporter)
+
+  std::fstream fs_;
+  std::string file_name_;
+
+public:
+  IndividualsFileReporter(const std::string& file_name);
+  //    IndividualsFileReporter(const IndividualsFileReporter& orig);
+  virtual ~IndividualsFileReporter();
+private:
+  void initialize() override;
+  void before_run() override;
+  void after_run() override;
+  void begin_time_step() override;
+  virtual void after_time_step();
+public:
+  void monthly_report() override;
+};
+
+#endif	/* INDIVIDUALSFILEREPORTER_H */
---
+++ src/DxGGenerator/PkPdReporter.h
@@ -0,0 +1,35 @@
+/* 
+ * File:   PkPdReporter.h
+ * Author: Merlin
+ *
+ * Created on October 29, 2014, 12:56 PM
+ */
+
+#ifndef PKPDREPORTER_H
+#define	PKPDREPORTER_H
+
+#include "Core/PropertyMacro.h"
+#include "Core/TypeDef.h"
+#include "Reporters/Reporter.h"
+
+
+class PkPdReporter : public Reporter {
+DISALLOW_COPY_AND_ASSIGN(PkPdReporter)
+PROPERTY_REF(DoubleVector, yesterday_density)
+public:
+  PkPdReporter();
+  //    PkPdReporter(const PkPdReporter& orig);
+  virtual ~PkPdReporter();
+
+  void initialize() override;
+  void before_run() override;
+  void after_run() override;
+  void begin_time_step() override;
+  virtual void after_time_step();
+
+  void monthly_report() override;
+private:
+
+};
+
+#endif	/* PKPDREPORTER_H */
