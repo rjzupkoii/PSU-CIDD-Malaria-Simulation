01dd27d032705eb723231fa2a681f522d97f81b1
Nguyen Tran <merlinvn@gmail.com>
2019-06-07 10:50:32 -0400

    update input flal mda

--- misc/MDA_WHO_ERG_2018/input_generator_FLAL.py
+++ misc/MDA_WHO_ERG_2018/input_generator_FLAL.py
@@ -34,7 +34,7 @@ data['location_db']['location_info']= location_info;
 
 
 #population size 
-popsize = 40000
+popsize = 300000
 data['location_db']['population_size_by_location'] = [popsize];       
 
 #3RMDA
@@ -49,13 +49,13 @@ data['sd_prob_individual_present_at_mda'] = sd_prob_individual_present_at_mda
 #        data['events'][index]['info'] = data['events'][index]['info'][0:number_MDA_round]
 
 
-betas = [0.1669, 0.076, 0.064, 0.058,0.053]
+betas = [0.1849, 0.077, 0.06413, 0.0585,0.0538]
 
-pfpr = {0.1669: 'PFPR15',        
-        0.076: 'PFPR5',
-        0.064: 'PFPR3',
-        0.058: 'PFPR2',
-        0.053: 'PFPR1'}
+pfpr = {0.1849: 'PFPR15',        
+        0.077: 'PFPR5',
+        0.06413: 'PFPR3',
+        0.0585: 'PFPR2',
+        0.0538: 'PFPR1'}
 
 improved_tc = {True: '_itc' , 
                False: ''}
