c85e5898b5cd85860426c557aa551c3c4023a158
rjzupkoii <rjzii@yahoo.com>
2019-09-06 18:02:04 -0400

    Work on DB proof-of-concept

--- src/Reporters/DbReporter.cpp
+++ src/Reporters/DbReporter.cpp
@@ -1,17 +1,50 @@
 #include "DbReporter.h"
 
-#include <pqxx/pqxx> 
+#include <fmt/format.h>
 
 #include "easylogging++.h"
+#include "Model.h"
 
 void DbReporter::initialize(int job_number, std::string path) {
+    // TODO Get this from the configuration file / command line?
+
     // Connect to the database
-    pqxx::connection* conn = new pqxx::connection("host=localhost dbname=masim user=sim password=sim");
+    conn = new pqxx::connection("host=localhost dbname=masim user=sim password=sim");
     LOG(INFO) << "Connected to " << conn->dbname();
 
-    // Work with it
+    // TODO Check the database to make sure it is what we want
+}
+
+void DbReporter::monthly_report() {
+    const std::string INSERT = 
+    "INSERT INTO sim.MonthlyData "
+    "(RunId, DaysElapsed, ModelTime, SeasonalFactor, TreatmentFailures, Beta) "
+    "VALUES ({}, {}, {}, {}, {}, {});";
+
+    // Prepare the query
+    auto run_id = 0;
+    auto days_elapsed = Model::SCHEDULER->current_time();
+    auto model_time = std::chrono::system_clock::to_time_t(Model::SCHEDULER->calendar_date);
+    auto seasonal_factor = Model::MODEL->get_seasonal_factor(Model::SCHEDULER->calendar_date, 0);
+    auto treatment_failures = 0;
+    auto beta = 0;
+    query += fmt::format(INSERT, run_id, days_elapsed, model_time, seasonal_factor, treatment_failures, beta);
+    count++;
+
+    // Run the query if it's been a year
+    if (count % 12 == 0) {
+        pqxx::work db(*conn);
+        db.exec(query);
+        db.commit();
+        query = "";
+        count = 0;
+    }
+}
+
+void DbReporter::after_run() {
+    // Run any final query
     pqxx::work db(*conn);
-    db.exec("INSERT INTO test (value) VALUES (1)");
+    db.exec(query);
     db.commit();
 
     // Close the connection 
--- src/Reporters/DbReporter.h
+++ src/Reporters/DbReporter.h
@@ -3,19 +3,26 @@
 
 #include "Reporter.h"
 
+#include <pqxx/pqxx> 
+
 class DbReporter : public Reporter {
-    public:
-      DbReporter() = default;
-      ~DbReporter() override = default;
+  private:
+    pqxx::connection* conn;
+    std::string query = "";
+    int count = 0;
 
-      // Basic declarations
-      void before_run() override {}
-      void begin_time_step() override {}
+  public:
+    DbReporter() = default;
+    ~DbReporter() override = default;
 
-      // Overrides
-      void initialize(int job_number, std::string path) override;
-      void monthly_report() override {}
-      void after_run() override {}
+    // Basic declarations
+    void before_run() override {}
+    void begin_time_step() override {}
+
+    // Overrides
+    void initialize(int job_number, std::string path) override;
+    void monthly_report() override;
+    void after_run() override;
 };
 
 #endif
