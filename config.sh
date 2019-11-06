38c68dd7c90efe5bdf498a6cf8c0263886a92658
rjzupkoii <rjzii@yahoo.com>
2019-11-06 15:23:38 -0500

    Started ACI config script

---
+++ config.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+function check_version() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
+
+# Create the directory to work in
+source=$(pwd)
+mkdir -p ~/work/build_env
+cd ~/work/build_env
+
+# If the version of git is to old, load a newer one
+required=2.24.0
+current=$(git --version | sed "s/git version //g")
+if check_version $required $current; then
+  echo "Upgrading git..."
+  git clone git://git.kernel.org/pub/scm/git/git.git
+  cd git
+  make configure
+  ./configure --prefix=/home
+  make all
+  export PATH="`pwd`:$PATH"
+  cd ..
+fi
+
+# If the version of cmake is too old, load a newer one
+required="3.15"
+current=$(cmake --version | sed "s/cmake version //g")
+if check_version $required $current; then
+  echo "Upgrading cmake..."
+  wget https://cmake.org/files/v3.15/cmake-3.15.0-Linux-x86_64.tar.gz
+  tar xf cmake-3.15.0-Linux-x86_64.tar.gz
+  rm cmake-3.15.0-Linux-x86_64.tar.gz
+  export PATH="`pwd`/cmake-3.15.0-Linux-x86_64/bin/:$PATH"
+fi
+
+# Return to the source directory
+cd $source
+
+# Prompt to update the bash profile
+echo $PATH
