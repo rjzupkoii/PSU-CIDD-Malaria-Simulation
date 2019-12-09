569f22b2e062ff969121ec11dfdba5cbd7c9b306
rjzupkoii <rjzii@yahoo.com>
2019-12-09 17:30:31 -0500

    Work on connecting to DB from Matlab

--- analysis/maps.m
+++ analysis/maps.m
@@ -1,21 +1,22 @@
 % maps.m
 %
-% Matlab function for generating ASC Files from the MaSim database.
+% Matlab function for generating ASC Files from the MaSim database,
+% developed targeting version 2019a.
 
-% TODO List out the relevant variables to connect
+% Relevant variables to connect
 DatabaseName = 'MaSim';
 UserName = 'sim';
 Password = 'sim';
-DatabaseURL = 'jdbc:postgresql://host:port/masim'
+
+% Test to make sure the JVM is running, install the driver to the path
+if ~usejava('jvm'); error([filename ' requires Java to run']); end
+javaaddpath(strcat(pwd, filesep, 'postgresql-42.2.9.jar'));
 
 % Open the connection to the database
-% https://www.mathworks.com/help/database/ug/database.html
-connection = database(DatabaseName, UserName, Password, 'org.postgresql.Driver', DatabaseURL)
-
-% Prepare the query
-query = 'SELECT * FROM sim.MonthlySiteData';
-data = select(connection, query);
-
-% TODO Convert the data into a matrix
-
-% TODO Write the data to a an ASC File
+connection = database('', 'sim', 'sim', 'org.postgresql.Driver', 'jdbc:postgresql://masimdb.vmhost.psu.edu:5432/masim');
+if connection.MaxDatabaseConnections == -1
+    error(['Error connecting to the database: ' connection.Message]);
+end
+    
+% Done, close the connection
+close(connection);
