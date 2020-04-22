083514fd02f079b6d8bea23ed3bbb242313d0824
rjzupkoii <rjzii@yahoo.com>
2020-04-21 22:59:30 -0400

    Bug fix

--- database/mmc/eir_vs_pfpr.sql
+++ database/mmc/eir_vs_pfpr.sql
@@ -1,8 +1,8 @@
 
 -- No treatment
 select replicateid,
-  cast(substr(filename, 5, position('-' in substr(filename, 5)) - 1) as int) as detection,
-  cast(replace(substr(filename, position('-' in substr(filename, 5)) + 5), '.yml', '') as float) as beta,
+  cast(substr(filename, 5, 4) as float) as transmission,
+  cast(replace(substr(filename, 10, 4), '.ym', '') as float) as beta,
   avg(eir) as eir, 
   avg(pfprunder5) as pfprunder5, 
   avg(pfpr2to10) as pfpr2to10, 
@@ -12,16 +12,14 @@ from sim.monthlydata md
   inner join sim.replicate r on r.id = replicateid
   inner join sim.configuration c on c.id = r.configurationid
 where dayselapsed > 7305 and dayselapsed < 7701
-  and replicateid in (
-	  select id from v_replicates where filename like 'mmc-%' and id not in (
-	  	select id from v_replicates where filename like 'mmc-%-treat-%'))
+  and replicateid in (select id from v_replicates where filename like 'mmc-0.55%')
 group by replicateid, filename
-order by detection, beta
+order by transmission, beta
 
 -- With treatment
 select replicateid,
-  cast(substr(filename, 5, position('-' in substr(filename, 5)) - 1) as int) as detection,
-  cast(replace(substr(filename, position('treat' in filename) + 6), '.yml', '') as float) as beta,
+  cast(substr(filename, 11, 4) as float) as transmission,
+  cast(replace(substr(filename, 16, 4), '.ym', '') as float) as beta,
   avg(eir) as eir, 
   avg(pfprunder5) as pfprunder5, 
   avg(pfpr2to10) as pfpr2to10, 
@@ -31,6 +29,6 @@ from sim.monthlydata md
   inner join sim.replicate r on r.id = replicateid
   inner join sim.configuration c on c.id = r.configurationid
 where dayselapsed > 7305 and dayselapsed < 7701
-  and replicateid in (select id from v_replicates where filename like 'mmc-%-treat-%')
+  and replicateid in (select id from v_replicates where filename like 'mmc-treat-0.55%')
 group by replicateid, filename
-order by detection, beta
+order by transmission, beta
