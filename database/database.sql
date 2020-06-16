f07f56653d3b1b88ae10d1c7aebfa1ee98a30f67
rjzupkoii <rjzii@yahoo.com>
2020-06-16 16:21:16 -0400

    Added sequence

--- database/database.sql
+++ database/database.sql
@@ -72,6 +72,15 @@ CREATE SEQUENCE sim.movement_id_seq
 
 ALTER SEQUENCE sim.movement_id_seq OWNER TO sim;
 
+CREATE SEQUENCE sim.notes_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1;
+
+ALTER SEQUENCE sim.notes_id_seq OWNER TO sim;
+
 CREATE SEQUENCE sim.replicate_id_seq
     INCREMENT 1
     START 1
