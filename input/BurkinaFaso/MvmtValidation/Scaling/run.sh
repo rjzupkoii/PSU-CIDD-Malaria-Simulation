aa8444566babb3896f8a9c52069413fe3488fb0b
rjzupkoii <rjzii@yahoo.com>
2020-06-25 17:25:57 -0400

    Clearing out some Burkina Faso settings

--- input/BurkinaFaso/MvmtValidation/Scaling/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in 0.01 0.05 0.10 0.15 0.20 0.25 0.50 0.75 10;
-do
-  sed 's/#SCALING#/'"$i"'/g' template.yml > scaling-$i.yml
-  sed 's/#SCALING#/'"$i"'/g' template.job > $i.pbs
-  qsub $i.pbs
-done
