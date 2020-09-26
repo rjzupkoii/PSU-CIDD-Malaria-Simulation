be3675cebe19a04221c03ab60170b862dcac6b21
rjzupkoii <rjzii@yahoo.com>
2020-09-26 02:39:08 -0400

    Performance tuning the stored procedures

--- database/stored_procedure.sql
+++ database/stored_procedure.sql
@@ -16,13 +16,15 @@ BEGIN
     RAISE NOTICE 'Culling replicate %', REPLICATE_ID;
   END IF;
 
+  -- Store the indicies as a temporary table
+  CREATE TEMP TABLE monthlydataidex AS
+  SELECT id FROM sim.monthlydata WHERE replicateid = REPLICATE_ID;
+
   -- Delete the genome data
-  DELETE FROM sim.monthlygenomedata WHERE monthlydataid IN (
-	  SELECT id FROM sim.monthlydata WHERE replicateid = REPLICATE_ID);
+  DELETE FROM sim.monthlygenomedata WHERE monthlydataid IN (SELECT id FROM monthlydataidex);
 
   -- Delete the site data
-  DELETE FROM sim.monthlysitedata WHERE monthlydataid IN (
-	  SELECT id FROM sim.monthlydata WHERE replicateid = REPLICATE_ID);
+  DELETE FROM sim.monthlysitedata WHERE monthlydataid IN (SELECT id FROM monthlydataidex);
 
   -- Delete the monthly data
   DELETE FROM sim.monthlydata WHERE replicateid = REPLICATE_ID;
@@ -33,6 +35,9 @@ BEGIN
   
   -- Delete the replicate entry
   DELETE FROM sim.replicate WHERE id = REPLICATE_ID;
+  
+  -- Drop the temporary table
+  DROP TABLE monthlydataidex;  
   
   -- Report complete
   RAISE NOTICE 'Complete';
@@ -46,19 +51,25 @@ AS $BODY$
 DECLARE
 record RECORD;
 BEGIN
-	-- Delete the replicate
-    FOR record IN select distinct r.id from sim.configuration c inner join sim.replicate r on r.configurationid = c.id where studyid = study_id LOOP
-        CALL delete_replicate(record.id);
+	-- Delete the replicates
+    FOR record IN select distinct r.id from sim.configuration c inner join sim.replicate r on r.configurationid = c.id where studyid = STUDY_ID LOOP
+        CALL delete_replicate(record.id, true);
     END LOOP;
 	
-	-- Delete the configuration
-	DELETE FROM sim.location WHERE configurationid IN (
-		SELECT id FROM sim.configuration WHERE studyid = study_id);
-	DELETE FROM sim.configuration WHERE studyid = study_id;
-	
+  	-- Store the indicies as a temporary table
+	CREATE TEMP TABLE configurationindex AS
+	SELECT id FROM sim.configuration WHERE studyid = STUDY_ID;
+
+	-- Delete the configurations
+	DELETE FROM sim.location WHERE configurationid IN (SELECT id FROM configurationindex);
+	DELETE FROM sim.configuration WHERE studyid = STUDY_ID;
+		
 	-- Delete the study
-	DELETE FROM sim.notes WHERE studyid = study_id;
-	DELETE FROM sim.study WHERE id = study_id;
+	DELETE FROM sim.notes WHERE studyid = STUDY_ID;
+	DELETE FROM sim.study WHERE id = STUDY_ID;
+	
+	-- Drop the temporary table
+  	DROP TABLE monthlydataidex;  
 	
 	-- Report complete
   	RAISE NOTICE 'Complete';
