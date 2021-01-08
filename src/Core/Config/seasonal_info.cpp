463caf2d7efaefd945e82750ff409a2189682aa5
rjzupkoii <rjzii@yahoo.com>
2021-01-08 15:08:14 -0500

    Corrected equation

--- src/Core/Config/seasonal_info.cpp
+++ src/Core/Config/seasonal_info.cpp
@@ -36,8 +36,8 @@ double seasonal_info::get_seasonal_factor(const date::sys_days &today, const int
 
   // Seasonal factor is determined by the algorithm:
   // 
-  // multiplier = base + (a * sin⁺(b * pi * (phi - t) / 365))
-  auto multiplier = a * sin(b * M_PI * (phi - day) / Constants::DAYS_IN_YEAR());
+  // multiplier = base + (a * sin⁺(b * pi * (t - phi) / 365))
+  auto multiplier = a * sin(b * M_PI * (day - phi) / Constants::DAYS_IN_YEAR());
   multiplier = (multiplier < 0) ? 0 : multiplier;
   multiplier += base;
 
