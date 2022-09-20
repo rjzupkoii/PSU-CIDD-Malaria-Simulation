92f80819c48186a56bb169cd8eeb6c6f1633a70a
rjzupkoii <rjzii@yahoo.com>
2022-09-20 15:42:02 -0400

    #70 checkpoint of code

--- src/Helpers/OSHelpers.h
+++ src/Helpers/OSHelpers.h
@@ -2,6 +2,9 @@
  * OSHelpers.hxx
  *
  * This header contains some useful functions that are intended to be cross-platform compatible.
+ *
+ * Code to get the physical and virtual memory used adapted from an answer on StackOverflow (CC BY-SA 4.0)
+ * https://stackoverflow.com/a/64166/1185
  */
 
 #ifndef OS_HXX
@@ -9,17 +12,26 @@
 
 #include <fstream>
 
-#include "sys/sysinfo.h"
-#include "sys/types.h"
-
 class OsHelpers {
- public:
-  static bool file_exists(const std::string &name) {
-    const std::ifstream f(name.c_str());
-    return f.good();
-  }
+  private:
+    static const int ERROR = -1;              // An error occurred during the operation
+    static const int NOT_SUPPORTED = -2;      // The operation is not supported by this OS
 
-  static int
+    // Parse a line from /proc/self/status to get the integer value
+    static int parseProcLine(char * line);
+
+    // Return the defined value in /proc/self/status
+    static int getProcValue(const char * name, size_t length);
+
+  public:
+    // Check to see if the file indicated exists.
+    static bool file_exists(const std::string &name);
+
+    // Get the physical memory that is currently used by this process, negative value indicates an error.
+    static int getPhysicalMemoryUsed();
+
+    // Get the virtual memory that is currently used by this process, negative value indicates an error.
+    static int getVirtualMemoryUsed();
 };
 
 #endif
