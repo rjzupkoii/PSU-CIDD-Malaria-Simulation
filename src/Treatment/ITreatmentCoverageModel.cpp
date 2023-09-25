5060bdd5227be809047087a022e8fd08da408c76
rjzupkoii <rjzii@yahoo.com>
2023-09-25 17:34:24 -0400

    Update to MIS convention, note assumptions in comments

--- src/Treatment/ITreatmentCoverageModel.cpp
+++ src/Treatment/ITreatmentCoverageModel.cpp
@@ -1,16 +1,25 @@
+/*
+ * ITreatmentCoverageModel.cpp
+ *
+ * Implement the functions for the treatment coverage model abstract class.
+ */
 #include "ITreatmentCoverageModel.h"
-#include "easylogging++.h"
+
 #include <yaml-cpp/yaml.h>
-#include "SteadyTCM.h"
+
 #include "Core/Config/Config.h"
-#include "LinearTCM.h"
+#include "easylogging++.h"
 #include "InflatedTCM.hxx"
+#include "LinearTCM.h"
+#include "SteadyTCM.h"
 
 double ITreatmentCoverageModel::get_probability_to_be_treated(const int &location, const int &age) {
-  LOG_IF(location < 0 || static_cast<std::size_t>(location) >= p_treatment_less_than_5.size() || 
-         static_cast<std::size_t>(location) >= p_treatment_more_than_5.size(), FATAL) << "wrong location value: " << location;
-         
-  return age <= 5 ? p_treatment_less_than_5[location] : p_treatment_more_than_5[location];
+  assert(location >= 0); // Location should not be less than zero
+  assert(static_cast<std::size_t>(location) >= p_treatment_less_than_5.size() ||
+         static_cast<std::size_t>(location) >= p_treatment_more_than_5.size());   // Location is greater than the array size
+
+  // This is abased upon the Malaria Indicator Survey, so we presume 0 - 59 months for under-5
+  return (age < 5) ? p_treatment_less_than_5[location] : p_treatment_more_than_5[location];
 }
 
 ITreatmentCoverageModel *ITreatmentCoverageModel::build_steady_tcm(const YAML::Node &node, Config *config) {
@@ -29,11 +38,10 @@ ITreatmentCoverageModel *ITreatmentCoverageModel::build_steady_tcm(const YAML::N
 
 void ITreatmentCoverageModel::read_p_treatment(const YAML::Node &node, std::vector<double> &p_treatments,
                                                const std::size_t number_of_locations) {
-  if (node) {
-    for (std::size_t loc = 0; loc < number_of_locations; loc++) {
-      std::size_t input_loc = node.size() < number_of_locations ? 0 : loc;
-      p_treatments.push_back(node[input_loc].as<float>());
-    }
+  if (!node) return;
+  for (std::size_t loc = 0; loc < number_of_locations; loc++) {
+    std::size_t input_loc = node.size() < number_of_locations ? 0 : loc;
+    p_treatments.push_back(node[input_loc].as<float>());
   }
 }
 
